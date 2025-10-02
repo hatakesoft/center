74ac3f4ac890a3acbce47f5ad02cfd14b06016dc
hatakesoft <hermit183@outlook.com>
2025-10-02 17:43:58 +0900

    add: quit event
    when: read q key

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use {
+    std:: {
+        io,
+        time,
+    },
+    crossterm:: {
+        cursor,
+        event,
+        execute,
+        terminal,
+    },
+};
+
+fn main() -> io::Result<()> {
+    terminal::enable_raw_mode()?;
+    execute!(
+        io::stdout(),
+        cursor::Hide,
+        terminal::EnterAlternateScreen,
+        cursor::MoveTo(0, 0),
+        terminal::Clear(terminal::ClearType::All),
+    )?;
+
+    loop {
+        if event::poll(time::Duration::from_millis(100))? {
+            match event::read()? {
+                event::Event::Key(key) => {
+                    match key.code {
+                        event::KeyCode::Char('q') => {
+                            break;
+                        }
+                        _ => {}
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+
+    terminal::disable_raw_mode()?;
+    execute!(
+        io::stdout(),
+        cursor::Show,
+        terminal::LeaveAlternateScreen,
+    )?;
+    Ok(())
 }
